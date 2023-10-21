5c6c249c6e0e43a5fe61ec3a881f922356e8c1fd
hudym <vadimgudym@gmail.com>
2023-10-22 01:38:25 +0300

    Init commit

---
+++ src/main.ts
@@ -0,0 +1,74 @@
+import * as core from '@actions/core';
+import {signAabFile, signApkFile} from "./signing";
+import path from "path";
+import fs from "fs";
+import * as io from "./io-utils";
+
+async function run() {
+  try {
+    if (process.env.DEBUG_ACTION === 'true') {
+      core.debug("DEBUG FLAG DETECTED, SHORTCUTTING ACTION.")
+      return;
+    }
+
+    const releaseDir = core.getInput('releaseDirectory');
+    const signingKeyBase64 = core.getInput('signingKeyBase64');
+    const alias = core.getInput('alias');
+    const keyStorePassword = core.getInput('keyStorePassword');
+    const keyPassword = core.getInput('keyPassword');
+    const customArgs = core.getInput("customArgs")
+
+    console.log(`Preparing to sign key @ ${releaseDir} with signing key`);
+
+    // 1. Find release files
+    const releaseFiles = io.findReleaseFiles(releaseDir);
+    if (releaseFiles !== undefined && releaseFiles.length !== 0) {
+      // 3. Now that we have a release files, decode and save the signing key
+      const signingKey = path.join(releaseDir, 'signingKey.jks');
+      fs.writeFileSync(signingKey, signingKeyBase64, 'base64');
+
+      // 4. Now zipalign and sign each one of the the release files
+      let signedReleaseFiles:string[] = [];
+      let index = 0;
+      for (let releaseFile of releaseFiles) {
+        core.debug(`Found release to sign: ${releaseFile.name}`);
+        const releaseFilePath = path.join(releaseDir, releaseFile.name);
+        let signedReleaseFile = '';
+        if (releaseFile.name.endsWith('.apk')) {
+          signedReleaseFile = await signApkFile(releaseFilePath, signingKey, alias, keyStorePassword, keyPassword, customArgs);
+        } else if (releaseFile.name.endsWith('.aab')) {
+          signedReleaseFile = await signAabFile(releaseFilePath, signingKey, alias, keyStorePassword, keyPassword, customArgs);
+        } else {
+          core.error('No valid release file to sign, abort.');
+          core.setFailed('No valid release file to sign.');
+        }
+
+        // Each signed release file is stored in a separate variable + output.
+        core.exportVariable(`SIGNED_RELEASE_FILE_${index}`, signedReleaseFile);
+        core.setOutput(`signedReleaseFile${index}`, signedReleaseFile);
+        signedReleaseFiles.push(signedReleaseFile);
+        ++index;
+      }
+
+      // All signed release files are stored in a merged variable + output.
+      core.exportVariable(`SIGNED_RELEASE_FILES`, signedReleaseFiles.join(":"));
+      core.setOutput('signedReleaseFiles', signedReleaseFiles.join(":"));
+      core.exportVariable(`NOF_SIGNED_RELEASE_FILES`, `${signedReleaseFiles.length}`);
+      core.setOutput(`nofSignedReleaseFiles`, `${signedReleaseFiles.length}`);
+
+      // When there is one and only one signed release file, stoire it in a specific variable + output.
+      if (signedReleaseFiles.length == 1) {
+        core.exportVariable(`SIGNED_RELEASE_FILE`, signedReleaseFiles[0]);
+        core.setOutput('signedReleaseFile', signedReleaseFiles[0]);
+      }
+      console.log('Releases signed!');
+    } else {
+      core.error("No release files (.apk or .aab) could be found. Abort.");
+      core.setFailed('No release files (.apk or .aab) could be found.');
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
---
+++ src/signing.ts
@@ -0,0 +1,129 @@
+import * as exec from '@actions/exec';
+import * as core from '@actions/core';
+import * as io from '@actions/io';
+import * as path from "path";
+import * as fs from "fs";
+
+export async function signApkFile(
+    apkFile: string,
+    signingKeyFile: string,
+    alias: string,
+    keyStorePassword: string,
+    keyPassword?: string,
+    customArgs?: string,
+): Promise<string> {
+
+    core.debug("Zipaligning APK file");
+
+    // Find zipalign executable
+    const buildToolsVersion = process.env.BUILD_TOOLS_VERSION || '29.0.3';
+    const androidHome = process.env.ANDROID_HOME;
+    const buildTools = path.join(androidHome!, `build-tools/${buildToolsVersion}`);
+    if (!fs.existsSync(buildTools)) {
+        core.error(`Couldnt find the Android build tools @ ${buildTools}`)
+    }
+
+    const zipAlign = path.join(buildTools, 'zipalign');
+    core.debug(`Found 'zipalign' @ ${zipAlign}`);
+
+    // Align the apk file
+    const alignedApkFile = apkFile.replace('.apk', '-aligned.apk');
+    await exec.exec(`"${zipAlign}"`, [
+        '-c',
+        '-v', '4',
+        apkFile
+    ]);
+
+    await exec.exec(`"cp"`, [
+        apkFile,
+        alignedApkFile
+    ]);
+
+    core.debug("Signing APK file");
+
+    // find apksigner path
+    const apkSigner = path.join(buildTools, 'apksigner');
+    core.debug(`Found 'apksigner' @ ${apkSigner}`);
+
+    // apksigner sign --ks my-release-key.jks --out my-app-release.apk my-app-unsigned-aligned.apk
+    const signedApkFile = apkFile.replace('.apk', '-signed.apk');
+    const args = [
+        'sign',
+        '--ks', signingKeyFile,
+        '--ks-key-alias', alias,
+        '--ks-pass', `pass:${keyStorePassword}`,
+        '--out', signedApkFile
+    ];
+
+    if (keyPassword) {
+        args.push('--key-pass', `pass:${keyPassword}`);
+    }
+
+    if (customArgs) {
+        const splitArgs = customArgs.split(',')
+        for (let i = 0; i++; i < splitArgs.length) {
+            if (i % 2 == 1) continue;
+
+            const keyOrSingleArg = splitArgs[i];
+            args.push(keyOrSingleArg);
+            const value = splitArgs[i + 1];
+            if (value.length > 0) {
+                args.push(value);
+            }
+        }
+    }
+
+    args.push(alignedApkFile);
+
+    await exec.exec(`"${apkSigner}"`, args);
+
+    // Verify
+    core.debug("Verifying Signed APK");
+    await exec.exec(`"${apkSigner}"`, [
+        'verify',
+        signedApkFile
+    ]);
+
+    return signedApkFile
+}
+
+export async function signAabFile(
+    aabFile: string,
+    signingKeyFile: string,
+    alias: string,
+    keyStorePassword: string,
+    keyPassword?: string,
+    customArgs?: string
+): Promise<string> {
+    core.debug("Signing AAB file");
+    const jarSignerPath = await io.which('jarsigner', true);
+    core.debug(`Found 'jarsigner' @ ${jarSignerPath}`);
+    const args = [
+        '-keystore', signingKeyFile,
+        '-storepass', keyStorePassword,
+    ];
+
+    if (keyPassword) {
+        args.push('-keypass', keyPassword);
+    }
+
+    if (customArgs) {
+        const splitArgs = customArgs.split(',')
+        for (let i = 0; i++; i < splitArgs.length) {
+            if (i % 2 == 1) continue;
+
+            const keyOrSingleArg = splitArgs[i];
+            args.push(keyOrSingleArg);
+            const value = splitArgs[i + 1];
+            if (value.length > 0) {
+                args.push(value);
+            }
+        }
+    }
+
+    args.push(aabFile, alias);
+
+    await exec.exec(`"${jarSignerPath}"`, args);
+
+    return aabFile
+}
